e2c01fd695ccc0c924c6f631824f26c23778b999
David Schnurr <schnurr@gmail.com>
2013-11-16 22:59:35 -0800

    Data

--- js/main.js
+++ js/main.js
@@ -5,15 +5,18 @@
 	$body = $('body'),
 	$win = $(window),
 	classes = window.classes,
-	teachers = window.teachers,
-	mode,
 	ROUTE_HOME = 1,
 	ROUTE_STUDIO = 2,
 	ROUTE_TEACHER = 3,
 	ROUTE_PLACE = 4,
-	ROUTE_CLASS = 5;
+	ROUTE_CLASS = 5,
+	mode = ROUTE_HOME;
 
 	function init() {
+		var path = window.location.pathname;
+		if (!path || path === '/') {
+			window.history.pushState({}, 'Divinitree | Omza', '/s/Divinitree');
+		}
 		switch_mode();
 	}
 
@@ -51,25 +54,27 @@
 
 	function load_classes (classes) {
 		var classList = $('.class-list').empty();
-		var html = '', teacher;
-		$.each(classes, function(){
-			teacher = teachers[this.teacher];
+		var html = '';
+		$.each(classes, function(i){
 			html += ''
-			+'<li class="class-li" data-id="'+this.i+'">'
+			+'<li class="class-li" data-id="'+this.class_id+'">'
 			+	'<div class="class-left">'
-			+		'<div class="class-icon" style="background-image:url('+teacher.img+')"></div>'
-			+		'<div class="class-sub">'+teacher.name+'</div>'
+			+		'<div class="class-icon" style="background-image:url('+(this.teacher_img || 'img/divinitree.jpg')+')"></div>'
+			+		'<div class="class-sub">'+this.teacher_name+'</div>'
 			+	'</div>'
 			+	'<div class="class-right">'
-			+		'<h3>'+this.title+'</h3>'
+			+		'<h3>'+this.class_name+'</h3>'
 			+		'<div class="class-rating" data-rating="'+((Math.random()*2)+3)+'"><div class="class-rating-i"></div></div>'
 			+		'<button type="button" class="btn btn-primary btn-class-details">See Class Details</button>'
 			+	'</div>'
 			+	'<div class="class-date">'
-			+		'<div class="class-day">'+this.day+'</div>'
-			+		'<div class="class-time">'+this.time+'</div>'
+			+		'<div class="class-day">'+this.class_day+'</div>'
+			+		'<div class="class-time">'+this.class_time+'</div>'
 			+	'</div>'
 			+'</li>';
+			if (i >= 35) {
+				return false;
+			}
 		});
 		classList.html(html);
 		classList.find('.class-rating').each(ratings);
@@ -78,7 +83,7 @@
 	function filterData () {
 		var results = [];
 		$.each(classes, function(i){
-			this.id = i;
+			this.class_id = i;
 			if (Math.random()*10 < (new Date()).getTime() % 10) {
 				results.push(this);
 			}
@@ -90,6 +95,25 @@
 		filterData();
 	}, 200));
 
+	$doc.on('click', '.home-search-btn', function(){
+		$body.addClass('mode-2');
+	});
+
+	$doc.on('click', '.class-li', function(){
+		var 
+		$this = $(this),
+		id = +$this.data('id'),
+		cls = classes[id];
+
+		var nice_name = cls.class_name.replace(/[^a-zA-Z0-9\-]+/g,'-');
+
+		window.history.pushState(
+			{}, 
+			cls.class_name + ' | ' + cls.studio + ' | ' + 'Omza', 
+			'/c/'+id+'/'+nice_name
+		);
+	});
+
 	function ratings() {
 		var 
 		$this = $(this),
@@ -100,12 +124,16 @@
 
 	$win.resize(function(){
 		var 
-		$panes = $('.pane'),
+		$panes = $('.pane, .sub-pane'),
 		top = $panes.first().offset().top;
 
 		$panes.css('height', $win.height() - top);
 	}).resize();
 
 	init();
+
+	setTimeout(function(){
+		$('.content-inner').addClass('t10');
+	},200);
 
 })(window, jQuery);
